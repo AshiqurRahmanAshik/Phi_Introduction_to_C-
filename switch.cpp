eda894bc72df007629b5f55dd153019dd3d7b0de
AshiqurRahmanAshik <ashiqurrahmanashik111@gmail.com>
2025-11-17 11:53:15 +0600

    Switch Case

--- switch.cpp
+++ switch.cpp
@@ -7,6 +7,16 @@ Problem Link: Switch
 using namespace std;
 
 int main() {
-     
+    int day;
+    cin >> day;
+    switch(day){
+        case 1: cout << "sat";break;
+        case 2: cout << "sun";break;
+        case 3: cout << "mon";break;
+        case 4: cout << "tue";break;
+        case 5: cout << "wed";break;
+        case 6: cout << "thr";break;
+        case 7: cout << "fri";break;
+    } 
     return 0;
 }
